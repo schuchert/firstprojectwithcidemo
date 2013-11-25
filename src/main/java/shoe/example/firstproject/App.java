198e9b0d5a1c3211db025101907c6946cbfbdf81
Brett L. Schuchert <schuchert@yahoo.com>
2013-11-25 13:50:19 -0600

    Minor update to see if Jenkins is picking up hooks.

--- src/main/java/shoe/example/firstproject/App.java
+++ src/main/java/shoe/example/firstproject/App.java
@@ -1,8 +1,7 @@
 package shoe.example.firstproject;
 
-public class App
-{
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
